44c958624e0f0818c539cac8698c7dcdc3eb0550
elladarte <rafaella.d.d.carvalho@gmail.com>
2021-02-25 18:31:57 -0300

    Curso Selenium Python

---
+++ selenium/aula_03_ex_01.py
@@ -0,0 +1,12 @@
+'''
+Exercicio 1
+
+Crie um programa com o Selenium
+
+- Gere um dicionario, onde a chave é a tag h1
+    O valor deve ser um dicionario 
+    Cada chave do valor deverá ser o valor de 'atributo'
+    Cada valor deve ser o texto contido no elemento
+
+https://curso-python-selenium.netlify.app/exercicio_01.html
+'''
