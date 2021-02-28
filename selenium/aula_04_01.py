81811462ec7d9a6bb2f5e626f325b0bc924b4f9c
elladarte <rafaella.d.d.carvalho@gmail.com>
2021-02-28 00:12:37 -0300

    Update 28 de fev

--- selenium/aula_04_01.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver import Firefox
-
-browser = Firefox()
-browser.get('https://selenium.dunossauro.live/aula_04_a.html')
-
-lista_n_ordenada = browser.find_element_by_tag_name('ul')
-lis = lista_n_ordenada.find_elements_by_tag_name('li')
-lis[0].find_element_by_tag_name('a').text
